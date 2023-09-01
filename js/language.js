bda754209d724856aca7f23a91e9b83a719b7eee
Malckyor <malckyormetal@gmail.com>
2023-09-01 09:12:44 +0900

    default

--- js/language.js
+++ js/language.js
@@ -18,7 +18,7 @@ btnToggleLanguage.addEventListener("click", () => {
 function loadPageContent() {
   const languageFolder = currentLanguage === "en" ? "html_en" : "html";
 
-  fetch(`/${languageFolder}/index.html`)
+  fetch(`${languageFolder}/index.html`)
     .then(response => response.text())
     .then(pageContent => {
       document.getElementById("main-content").innerHTML = pageContent;
